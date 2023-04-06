3870cd88e8cadfb153f81391f907d0261f406db0
Pooja-sisodia <poojasisodia406@gmail.com>
2023-04-06 11:26:17 +0530

    working table

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import QuizComp from './exam/QuizComp'
+import TableGenerator from './TableGenerator';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
     root.render(
-      <React.StrictMode>
-      <QuizComp />
-      </React.StrictMode>
+     <TableGenerator />
     );
   
 
